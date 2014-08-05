c93b63194749cbf4a7ca83ad189de0b6689b8059
Bill Rowell <billr578@gmail.com>
2014-08-05 14:29:54 -0400

    base service request to shipwire

---
+++ lib/shipwire/service_request.rb
@@ -0,0 +1,40 @@
+require 'faraday'
+
+module Shipwire
+  class ServiceRequest
+    attr_accessor :payload, :api_path
+    attr_reader :response, :errors
+
+    def initialize(params={})
+      @errors = []
+    end
+
+    def send
+      begin
+        connection = Faraday.new(url: Shipwire.endpoint) do |faraday|
+          faraday.request :url_encoded
+          faraday.adapter Faraday.default_adapter
+        end
+
+        @response = connection.post @api_path, @payload
+      rescue Faraday::ConnectionFailed
+        @errors << 'Unable to connect to Shipwire'
+      rescue
+        @errors << 'Unknown error'
+      end
+    end
+
+    def is_ok?(element)
+      element.xpath('//Status').text == 'OK'
+    end
+
+    def parse_response
+      raise 'Override this'
+    end
+
+    protected
+    def build_payload
+      raise 'Override this'
+    end
+  end
+end
