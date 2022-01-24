0918483233e5402607f53d398397c8f25fdeea7c
Eric Ellingson <eellingson@bluebottlecoffee.com>
2022-01-24 12:48:28 -0600

    remove unrelated changes from shipwire/orders.rb

--- lib/shipwire/orders.rb
+++ lib/shipwire/orders.rb
@@ -24,16 +24,8 @@ module Shipwire
       request(:get, "orders/#{id}/holds", params: params)
     end
 
-    def release(id)
-      request(:post, "orders/#{id}/holds/clear")
-    end
-
     def items(id)
       request(:get, "orders/#{id}/items")
-    end
-
-    def pieces(id)
-      request(:get, "orders/#{id}/pieces")
     end
 
     def returns(id)
